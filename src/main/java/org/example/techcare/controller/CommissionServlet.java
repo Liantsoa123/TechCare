f69deb0270ecea90e3e588184935baabe734ca1c
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-21 18:14:22 +0300

    feat: create CommissionServlet contoller.

--- src/main/java/org/example/techcare/controller/CommissionServlet.java
+++ src/main/java/org/example/techcare/controller/CommissionServlet.java
@@ -10,9 +10,13 @@ import org.example.techcare.model.component.TypeComponent;
 import org.example.techcare.dao.TypeComponentDAO;
 import org.example.techcare.model.repair.Repair;
 import org.example.techcare.dao.RepairDAO;
+import org.example.techcare.dto.CommissionPeriod;
+
+
 
 import java.io.IOException;
-import java.util.Date;
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "CommissionServlet", value = "/commissionServlet")
@@ -21,28 +25,35 @@ public class CommissionServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
+        request.setAttribute("page", "CommissionParVendeur");
 
-        List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
-        request.setAttribute("TypeComponent", listTypeComponents);
-        request.setAttribute("page", "listRepair");
         RequestDispatcher dispastcher = request.getRequestDispatcher("index.jsp");
         dispastcher.forward(request, response);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Date dateDebut = req.getParameter("dateDebut");
-        Date dateFin = req.getParameter("dateFin");
+        Date dateDebut = java.sql.Date.valueOf(req.getParameter("dateDebut"));
+        Date dateFin = java.sql.Date.valueOf(req.getParameter("dateFin"));
+
+        System.out.println("huhu commssion ");
+        List<CommissionPeriod> commissionPeriods = new ArrayList<>();
+        commissionPeriods = CommissionPeriod.getCommissionByDateDebutAndDateFin(dateDebut , dateFin);
 
 
-        List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
-        req.setAttribute("TypeComponent", listTypeComponents);
-        req.setAttribute("page", "listRepair");
+        req.setAttribute("commissions" , commissionPeriods);
+
+
+        System.out.println("commission" + commissionPeriods.size());
+        for ( CommissionPeriod commissionPeriod : commissionPeriods ){
+            System.out.println("comm = " + commissionPeriod.getTechnician().getName());
+        }
+
+        req.setAttribute("page", "CommissionParVendeur");
 
         RequestDispatcher dispastcher = req.getRequestDispatcher("index.jsp");
         dispastcher.forward(req, resp);
-    }
 
-    public void destroy() {
     }
 }
