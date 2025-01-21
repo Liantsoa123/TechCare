5b581eaa1ca600cde59a895083fd7a5065e2db2d
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-21 20:58:56 +0300

    feat: add the condition to insert repair in the controller RepairServlet.

--- src/main/java/org/example/techcare/controller/RepairServlet.java
+++ src/main/java/org/example/techcare/controller/RepairServlet.java
@@ -6,12 +6,18 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.techcare.dao.*;
 import org.example.techcare.model.component.TypeComponent;
-import org.example.techcare.dao.TypeComponentDAO;
+import org.example.techcare.model.laptop.Laptop;
 import org.example.techcare.model.repair.Repair;
-import org.example.techcare.dao.RepairDAO;
+import org.example.techcare.model.repair.RepairStatus;
+import org.example.techcare.model.repair.RepairType;
+import org.example.techcare.model.technician.Technician;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 @WebServlet(name = "RepaireServlet", value = "/repaireServlet")
@@ -20,26 +26,57 @@ public class RepairServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
+        String mode = request.getParameter("mode");
+        if (mode != null && mode.equals("insert")) {
+            List<RepairType> repairTypeList = new RepairTypeDAO().getAllRepairTypes();
+            List<Technician> technicians = new TechnicianDAO().getAllTechnicians();
+            List<Laptop> laptopList = new LaptopDAO().getAllLaptops();
 
-        List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
-        request.setAttribute("TypeComponent", listTypeComponents);
-        request.setAttribute("page", "listRepair");
+            request.setAttribute("repairTypeList", repairTypeList);
+            request.setAttribute("technicianList", technicians);
+            request.setAttribute("laptopList", laptopList);
+            request.setAttribute("page", "insertRepair");
+
+        } else {
+            List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
+            request.setAttribute("TypeComponent", listTypeComponents);
+            request.setAttribute("page", "listRepair");
+        }
+
         RequestDispatcher dispastcher = request.getRequestDispatcher("index.jsp");
         dispastcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int idTypeComponent = Integer.parseInt(req.getParameter("typComponentId"));
-        List<Repair> repairs = new RepairDAO().getRepairsByTypeComponentId(idTypeComponent);
-        req.setAttribute("repairs", repairs);
+        String mode = req.getParameter("mode");
+//       Insertion repair
+        if (mode != null && mode.equals("insert")) {
+            Laptop laptop = new LaptopDAO().getLaptopById(Integer.parseInt(req.getParameter("laptopId")));
+            RepairType repairType = new RepairTypeDAO().getRepairTypeById(Integer.parseInt(req.getParameter("repairTypeId")));
+            Technician technician = new TechnicianDAO().getTechnicianById(Integer.parseInt(req.getParameter("technicianId")));
+            Timestamp dateRepair = new Timestamp(System.currentTimeMillis());
+            BigDecimal total = req.getParameter("total") != null ? new BigDecimal(req.getParameter("total")) : new BigDecimal(0);
+            RepairStatus repairStatus = new RepairStatusDAO().getRepairStatusById(1);
+            Repair repair = new Repair(0,dateRepair, null, laptop, technician, repairStatus , total, repairType);
+            new RepairDAO().createRepair(repair);
+            resp.sendRedirect("repaireServlet");
+            return;
+        }
+//        Search Repair by TypeComponent ID
+        else {
+            int idTypeComponent = Integer.parseInt(req.getParameter("typComponentId"));
+            List<Repair> repairs = new RepairDAO().getRepairsByTypeComponentId(idTypeComponent);
+            req.setAttribute("repairs", repairs);
+            List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
+            req.setAttribute("TypeComponent", listTypeComponents);
 
-        List<TypeComponent> listTypeComponents = new TypeComponentDAO().getAllTypeComponents();
-        req.setAttribute("TypeComponent", listTypeComponents);
-        req.setAttribute("page", "listRepair");
+            req.setAttribute("page", "listRepair");
 
-        RequestDispatcher dispastcher = req.getRequestDispatcher("index.jsp");
-        dispastcher.forward(req, resp);
+            RequestDispatcher dispastcher = req.getRequestDispatcher("index.jsp");
+            dispastcher.forward(req, resp);
+
+        }
     }
 
     public void destroy() {
