a0a8665f1d5c0c595ebc1d26086334f4910e597b
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-23 15:43:55 +0300

    fix: fix bug.

--- src/main/java/org/example/techcare/dto/CommissionPeriod.java
+++ src/main/java/org/example/techcare/dto/CommissionPeriod.java
@@ -3,6 +3,7 @@ package org.example.techcare.dto;
 import org.example.techcare.model.technician.Technician;
 import org.example.techcare.dao.TechnicianDAO;
 import org.example.techcare.model.utils.ConnectionBdd;
+
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
@@ -36,7 +37,7 @@ public class CommissionPeriod {
         this.total = total;
     }
 
-    public static List<CommissionPeriod> getCommissionByDateDebutAndDateFin( Date dateDebut, Date dateFin) {
+    public static List<CommissionPeriod> getCommissionByDateDebutAndDateFin(Date dateDebut, Date dateFin) {
         List<CommissionPeriod> commissionPeriods = new ArrayList<>();
         String sql = "SELECT technician_id, (SUM(total) * 5 / 100 ) AS total_amount FROM repair " +
                 "WHERE  DATE(filing_date) >= ? AND DATE(filing_date) <= ? " +
@@ -49,7 +50,7 @@ public class CommissionPeriod {
 
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
-                    Technician  technician = new TechnicianDAO().getTechnicianById( resultSet.getInt("technician_id") );
+                    Technician technician = new TechnicianDAO().getTechnicianById(resultSet.getInt("technician_id"));
                     BigDecimal totalAmount = resultSet.getBigDecimal("total_amount");
                     CommissionPeriod commissionPeriod = new CommissionPeriod(technician, totalAmount);
                     commissionPeriods.add(commissionPeriod);
@@ -62,44 +63,72 @@ public class CommissionPeriod {
         return commissionPeriods;
     }
 
-public static List<CommissionPeriod> getCommissionByDateDebutAndDateFin( Date dateDebut, Date dateFin, int id_sexe , String montant) {
-    List<CommissionPeriod> commissionPeriods = new ArrayList<>();
-    String sql ;
-    Double montan = 0.0; 
-    if ( montant ==null || montant.isEmpty() ){
-         sql = "SELECT repair.technician_id as technician_id , (SUM(total) * 5 / 100 ) AS total_amount FROM repair\n" +
+
+    public static List<CommissionPeriod> getCommissionByDateDebutAndDateFin(Date dateDebut, Date dateFin, int id_sexe, String montant) {
+        List<CommissionPeriod> commissionPeriods = new ArrayList<>();
+        String sql;
+        Double montan = 0.0;
+        if (montant == null || montant.isEmpty()) {
+            sql = "SELECT repair.technician_id as technician_id , (SUM(total) * 5 / 100 ) AS total_amount FROM repair\n" +
+                    "            join technician t on  repair.technician_id = t.technician_id\n" +
+                    "            WHERE  DATE(filing_date) >= ? AND DATE(filing_date) <= ? AND t.id_sexe = ? \n" +
+                    "            GROUP BY repair.technician_id";
+
+        } else {
+            montan = Double.valueOf(montant);
+            sql = "SELECT repair.technician_id AS technician_id, (SUM(total) * 5 / 100) AS total_amount FROM repair JOIN technician t ON repair.technician_id = t.technician_id WHERE DATE(filing_date) >= ? AND DATE(filing_date) <= ? AND t.id_sexe = ? GROUP BY repair.technician_id HAVING (SUM(total)) >= ?";
+        }
+
+        try (Connection connection = new ConnectionBdd().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setDate(1, dateDebut);
+            statement.setDate(2, dateFin);
+            statement.setInt(3, id_sexe);
+            if (montan > 0) {
+                statement.setDouble(4, montan);
+            }
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Technician technician = new TechnicianDAO().getTechnicianById(resultSet.getInt("technician_id"));
+                    BigDecimal totalAmount = resultSet.getBigDecimal("total_amount");
+                    CommissionPeriod commissionPeriod = new CommissionPeriod(technician, totalAmount);
+                    commissionPeriods.add(commissionPeriod);
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error while retrieving commission: " + e.getMessage());
+        }
+
+        return commissionPeriods;
+    }
+
+    public static List<CommissionPeriod> getCommissionByDateDebutAndDateFin(Date dateDebut, Date dateFin, int id_sexe) {
+        List<CommissionPeriod> commissionPeriods = new ArrayList<>();
+        String sql;
+        sql = "SELECT repair.technician_id as technician_id , (SUM(total) * 5 / 100 ) AS total_amount FROM repair\n" +
                 "            join technician t on  repair.technician_id = t.technician_id\n" +
                 "            WHERE  DATE(filing_date) >= ? AND DATE(filing_date) <= ? AND t.id_sexe = ? \n" +
                 "            GROUP BY repair.technician_id";
-
-    }else {
-        montan = Double.valueOf(montant);
-        sql = "SELECT repair.technician_id AS technician_id, (SUM(total) * 5 / 100) AS total_amount FROM repair JOIN technician t ON repair.technician_id = t.technician_id WHERE DATE(filing_date) >= ? AND DATE(filing_date) <= ? AND t.id_sexe = ? GROUP BY repair.technician_id HAVING (SUM(total) * 5 / 100) >= ?";
-    }
-    
-    try (Connection connection = new ConnectionBdd().getConnection();
-         PreparedStatement statement = connection.prepareStatement(sql)) {
-        statement.setDate(1, dateDebut);
-        statement.setDate(2, dateFin);
-        statement.setInt(3,id_sexe);
-        if ( montan > 0  ){
-            statement.setDouble(4, montan);
-        }
-
-        try (ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                Technician  technician = new TechnicianDAO().getTechnicianById( resultSet.getInt("technician_id") );
-                BigDecimal totalAmount = resultSet.getBigDecimal("total_amount");
-                CommissionPeriod commissionPeriod = new CommissionPeriod(technician, totalAmount);
-                commissionPeriods.add(commissionPeriod);
+        try (Connection connection = new ConnectionBdd().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setDate(1, dateDebut);
+            statement.setDate(2, dateFin);
+            statement.setInt(3, id_sexe);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Technician technician = new TechnicianDAO().getTechnicianById(resultSet.getInt("technician_id"));
+                    BigDecimal totalAmount = resultSet.getBigDecimal("total_amount");
+                    CommissionPeriod commissionPeriod = new CommissionPeriod(technician, totalAmount);
+                    commissionPeriods.add(commissionPeriod);
+                }
             }
+        } catch (SQLException e) {
+            System.err.println("Error while retrieving commission: " + e.getMessage());
         }
-    } catch (SQLException e) {
-        System.err.println("Error while retrieving commission: " + e.getMessage());
-    }
 
-    return commissionPeriods;
-}
+        return commissionPeriods;
+    }
 
 }
 
